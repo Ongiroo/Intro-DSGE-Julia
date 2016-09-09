86339080f4182e826e4054b30488a261f26d22a6
ongiroo <amgalan.amg@gmail.com>
2016-09-09 22:03:01 +0100

    Demo1

--- run_default.jl
+++ run_default.jl
@@ -1,23 +1,10 @@
-# julia 0.4
-# Pkg.add("Calculus")
-# Pkg.add("DataStructures")
-# Pkg.add("FredData")
-# Pkg.add("HDF5")
-# Pkg.add("Optim")
-# Pkg.add("QuantEcon")
-# Pkg.add("Roots")
-Pkg.add("DSGE")
-Pkg.update()
-Pkg.status()
-Pkg.rm("Plots")
-# Pkg.clone(https://github.com/FRBNY-DSGE/DSGE.jl.git, Actors)
 using DSGE
 
 # construct a model object
 m = Model990()
 
 # estimate as of 2015-Q3 using the default data vintage from 2015 Nov 27
-m <= Setting(:data_vintage, "2")
+m <= Setting(:data_vintage, "151127")
 m <= Setting(:date_mainsample_end, quartertodate("2015-Q3"))
 
 # reoptimize parameter vector, compute Hessian at mode, and full posterior
